62ad6202cc9274096b684ed476c88d493df25081
Justin Etzine <justetz@gmail.com>
2015-10-19 17:49:30 -0400

    Got the sliders working

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-function main() {
-	newColor();
-	$("#sliders").children().slider({
+$(document).ready(function() {
+	var sliderObject = {
 		min:0,
 		max:255,
-		slide: function(event, ui) {}
-	});
-	var values = $("#sliders").children().slider("value");
+		slide: function(event, ui) {
+			$('#' + event.target.id + 'Val').html($("#"+event.target.id).slider("option", "value"));
+		}
+	};
 
+	$( "#red" ).slider(sliderObject);
+	$( "#green" ).slider(sliderObject);
+	$( "#blue" ).slider(sliderObject);
+});
+
+/*
+<script>
+var rvalue = $("#red").slider("option", "value");
+var gvalue = $("#green").slider("option", "value");
+var bvalue = $("#blue").slider("option", "value");
+</script>*/
+
+function main() {
+	newColor();
 }
 
 function newColor() {
@@ -14,4 +28,3 @@ function newColor() {
 	var color = '#'+Math.floor(Math.random()*16777215).toString(16);
 	document.getElementById("color").style.backgroundColor = color;
 }
-
